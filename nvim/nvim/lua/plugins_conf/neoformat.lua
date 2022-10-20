7754cafc3b0811df54b0cbf20004005d79a5accf
paraparata <srizalmp@gmail.com>
2022-10-20 19:33:47 +0800

    update nvim, starship, alacritty, and alias configs

--- nvim/nvim/lua/plugins_conf/neoformat.lua
+++
@@ -1,3 +0,0 @@
-require('sbdchd/neoformat').setup()
-
-vim.g.neoformat_try_node_exe = 1
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,19 @@
 alias nv="nvim"
+
+# git
+alias gsws="gsw staging"
+alias gfsps="gf && gsws && git pull"
 alias gfspd="gf && gswd && git pull"
 alias gssw="git stash --include-untracked && git switch"
+alias gbDxp='f() { git branch | grep $1 };f'
+alias gbDx='f() { git branch | grep $1 | xargs git branch -D };f'
+
+# rust
 alias carn="cargo new"
 alias carr="cargo run"
 alias carb="cargo build"
+
+# nx monorepo
+# function nxgn () {
+#  nx g @nrwl/next:$1 
+# }
